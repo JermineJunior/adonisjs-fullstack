a9ae2187edc77e1625c74c1133b15eb9dfa4d98d
Jermaine Junior <obietrice734@gmail.com>
2024-12-09 21:55:45 +0200

    registeration feature implemented

--- app/controllers/register_controller.ts
+++ app/controllers/register_controller.ts
@@ -1,9 +1,27 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { RegisterValidator } from '../validators/register.js'
+import User from '../models/user.js'
 
 export default class RegisterController {
   create({ view }: HttpContext) {
     return view.render('pages/auth/register')
   }
 
-  store({ request, auth, session, response }) {}
+  async store({ request, auth, session, response }: HttpContext) {
+    //validate the user data
+    const validatedData = await request.validateUsing(RegisterValidator)
+    // create a new user
+    const user = await User.create(validatedData)
+    // login the newly created user
+    await auth.use('web').login(user)
+
+    session.flash({
+      notification: {
+        type: 'Sucess',
+        message: 'Regestration was done succesfuly',
+      },
+    })
+
+    return response.redirect('/')
+  }
 }
